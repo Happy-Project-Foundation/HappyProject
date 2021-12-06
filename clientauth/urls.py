ed512d7b6991d982caaa2019f9a2621bdc6680c7
BirnadinErick <45619033+BirnadinErick@users.noreply.github.com>
2021-12-06 21:21:05 +0530

    Done HomeView: Howtos

--- clientauth/urls.py
+++ clientauth/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from clientauth.views import auth_view
 
+app_name = "clientauth"
 urlpatterns = [
-    path('', auth_view, name="auth_test"),
+    path('', auth_view, name="index"),
 ]
