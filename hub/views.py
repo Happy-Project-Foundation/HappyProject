84437fe49a2514d9c9f0b7d36a829151eac6a08d
Birnadin Erick <45619033+BirnadinErick@users.noreply.github.com>
2022-03-22 09:55:11 +0530

    Hotfix

--- hub/views.py
+++ hub/views.py
@@ -33,63 +33,63 @@ def hub_index(req):
 
 class StudentHubView(ListView):
     template_name = "hub/student_hub.html"
-    paginate_by = 15
-    model = Resource
-    ordering = ["title"]
+#     paginate_by = 15
+#     model = Resource
+#     ordering = ["title"]
 
     # student specific attr(s)
-    subjects = None
-    resources = None
-    motto = None
-    name = None
+#     subjects = None
+#     resources = None
+#     motto = None
+#     name = None
 
-    def __int__(self):
-        self._prepare_student()
+#     def __int__(self):
+#         self._prepare_student()
 
-    def get_queryset(self):
+#     def get_queryset(self):
 
-        try:
-            keys = self.request.GET.keys()
-            if "type" in keys:
-                queryset = Resource.objects.filter(
-                    type__icontains=self.request.GET["type"]).order_by("title")
-            elif "subject" in keys:
-                queryset = Resource.objects.filter(
-                    subject__icontains=self.request.GET["subject"]).order_by("title")
-            elif "search" in keys:
-                # TODO: search within description also
-                queryset = Resource.objects.filter(
-                    title__icontains=self.request.GET["search"]).order_by("title")
-            else:
-                queryset = super().get_queryset().order_by("title")
+#         try:
+#             keys = self.request.GET.keys()
+#             if "type" in keys:
+#                 queryset = Resource.objects.filter(
+#                     type__icontains=self.request.GET["type"]).order_by("title")
+#             elif "subject" in keys:
+#                 queryset = Resource.objects.filter(
+#                     subject__icontains=self.request.GET["subject"]).order_by("title")
+#             elif "search" in keys:
+#                 # TODO: search within description also
+#                 queryset = Resource.objects.filter(
+#                     title__icontains=self.request.GET["search"]).order_by("title")
+#             else:
+#                 queryset = super().get_queryset().order_by("title")
 
-        except:
-            raise HttpResponseServerError()
+#         except:
+#             raise HttpResponseServerError()
 
-        else:
-            return queryset
+#         else:
+#             return queryset
 
     def get_context_data(self, **kwargs):
-        self._prepare_student()
+#         self._prepare_student()
         context = super().get_context_data(**kwargs)
         # NOTE: Test var.
         context["page_title"] = "Happy Hub | Happy Project 😊"
 
         # trending blogs construction
-        context["blogs"] = Blog.objects.all().order_by("title")[:8]
+#         context["blogs"] = Blog.objects.all().order_by("title")[:8]
 
         # student's
-        context["subjects"] = self.subjects
-        context["resources"] = self.resources
-        context["motto"] = self.motto
-        context["name"] = self.name
+#         context["subjects"] = self.subjects
+#         context["resources"] = self.resources
+#         context["motto"] = self.motto
+#         context["name"] = self.name
 
-        print(context)
+#         print(context)
         return context
 
-    def _prepare_student(self):
-        student = Student.objects.get(student=self.request.user)
-        self.subjects = student.subjects.split(",")
-        self.resources = student.resources.split(",")
-        self.motto = student.motto
-        self.name = student.student.get_full_name()
+#     def _prepare_student(self):
+#         student = Student.objects.get(student=self.request.user)
+#         self.subjects = student.subjects.split(",")
+#         self.resources = student.resources.split(",")
+#         self.motto = student.motto
+#         self.name = student.student.get_full_name()
