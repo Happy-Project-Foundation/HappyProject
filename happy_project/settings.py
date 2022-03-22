7bd3beb06b97ee7da2d13331a145a38a2a11712c
Birnadin Erick <45619033+BirnadinErick@users.noreply.github.com>
2022-03-22 09:38:30 +0530

    Update settings.py ALLOWED_HOSTS

--- happy_project/settings.py
+++ happy_project/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = 'happyProj3ctbyShantho$h&B1rnadin3rick'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["happyprojectapp.herokuapp.com"]
+ALLOWED_HOSTS = ["happyproject.birnadine.guru", "happyprojectapp.herokuapp.com"]
 
 INSTALLED_APPS = [
     # my apps
